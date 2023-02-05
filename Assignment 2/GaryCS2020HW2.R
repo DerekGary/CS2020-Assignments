5983ef961b7cc8bef0bf2bd50951cfe6bcfa95fe
DerekGary <jgary4151@gmail.com>
2023-02-04 21:21:43 -0700

    Add output to comments

--- Assignment 2/GaryCS2020HW2.R
+++ Assignment 2/GaryCS2020HW2.R
@@ -9,20 +9,30 @@ Auto <- read.csv("Auto.csv")
 
 # b) Finding Type and Class of Auto
 #
+#Output:
+#[1] "list"
+#[1] "data.frame"
+
 print(typeof(Auto))
 print(class(Auto))
 
 # c) Get the Dimensions of Auto dataframe
 #
+#Output: 
+#[1] 397   9
 print(dim(Auto))
 
 # d) Remove all NA’s from Auto variable and redo step c
 #
+#Output:
+#[1] 397   9
 Auto <- na.omit(Auto)
 print(dim(Auto))
 
 # e) Report the names of all variables in Auto
 #
+#Output:
+#[1] "mpg"          "cylinders"    "displacement" "horsepower"   "weight"       "acceleration" "year"         "origin"       "name" 
 print(names(Auto))
 
 # f) Direct field access and then plot cylinder by mpg
@@ -49,7 +59,7 @@ detach(Auto)
 # and then converted these values to numerical.
 # Only then was I able to complete the pairs operation on this data.
 # I don't know if this was the right sets of operations to perform
-# since we omitted NAs earlier.
+# since we omitted NAs earlier. However, checks for 
 #
 Auto$horsepower[Auto$horsepower == "?"] <- NA
 Auto$horsepower <- as.numeric(Auto$horsepower)
@@ -59,3 +69,12 @@ pairs(Auto[, scatter_variables])
 # j) Report descriptive statistics for all fields in Auto
 #
 summary(Auto)
+# Output:
+#      mpg        cylinders  displacement     horsepower        weight      acceleration        year           origin          name          
+#Min.   : 9.00   3:  4     Min.   : 68.0   Min.   : 46.0   Min.   :1613   Min.   : 8.00   Min.   :70.00   Min.   :1.000   Length:397        
+#1st Qu.:17.50   4:203     1st Qu.:104.0   1st Qu.: 75.0   1st Qu.:2223   1st Qu.:13.80   1st Qu.:73.00   1st Qu.:1.000   Class :character  
+#Median :23.00   5:  3     Median :146.0   Median : 93.5   Median :2800   Median :15.50   Median :76.00   Median :1.000   Mode  :character  
+#Mean   :23.52   6: 84     Mean   :193.5   Mean   :104.5   Mean   :2970   Mean   :15.56   Mean   :75.99   Mean   :1.574                     
+#3rd Qu.:29.00   8:103     3rd Qu.:262.0   3rd Qu.:126.0   3rd Qu.:3609   3rd Qu.:17.10   3rd Qu.:79.00   3rd Qu.:2.000                     
+#Max.   :46.60             Max.   :455.0   Max.   :230.0   Max.   :5140   Max.   :24.80   Max.   :82.00   Max.   :3.000                     
+#NA's   :5                                                                       
